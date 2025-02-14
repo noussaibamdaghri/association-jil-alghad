fc2e51ef3b1c59e9669105aac16c47adee069fe8
noussaibamdaghri <160670962+noussaibamdaghri@users.noreply.github.com>
2025-02-14 17:01:19 +0100

    Update server.js

--- jil-alghad-backend/server.js
+++ jil-alghad-backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config(); // Charge les variables d'environnement depuis .env
 const express = require('express');
 const connectDB = require('./config/db');
 const newsRoutes = require('./routes/newsRoutes');
+const cors = require("cors");
 
 const app = express();
 
@@ -14,18 +15,17 @@ connectDB();
 // Middleware pour analyser les requêtes JSON
 app.use(express.json());
 
+// Configuration de CORS
+app.use(cors({
+    origin: ["https://jil-alghad-admin-3r38aqyu1-noussaiba-mdaghris-projects.vercel.app", "https://noussaibamdaghri.github.io/association-jil-alghad"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
 // Routes
 app.use('/api/news', newsRoutes); // Montez les routes ici
 
 // Serveur
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
-
-const cors = require("cors");
-
-app.use(cors({
-    origin: [" https://jil-alghad-admin-3r38aqyu1-noussaiba-mdaghris-projects.vercel.app", "https://noussaibamdaghri.github.io/association-jil-alghad"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
 
